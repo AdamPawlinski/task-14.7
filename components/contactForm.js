7a64789db4dd9ce8e3fb89e2eccee8b116e8a05a
Adam Pawliński <adam.pawlinski@gmail.com>
2018-04-23 07:27:44 +0200

    task 14.5 changed for JSX syntax

--- components/contactForm.js
+++ components/contactForm.js
@@ -5,24 +5,29 @@ var ContactForm = React.createClass({
 
   render: function() {
     return (
-      React.createElement('form', {className: 'contactForm'},
-        React.createElement('input', {
-          type: 'text',
-          placeholder: 'Name',
-          value: this.props.contact.firstName
-        }),
-        React.createElement('input', {
-          type: 'text',
-          placeholder: 'Last name',
-          value: this.props.contact.lastName
-        }),
-        React.createElement('input', {
-          type: 'text',
-          placeholder: 'email',
-          value: this.props.contact.email
-        }),
-        React.createElement('button', {type: 'submit'}, 'Add contact')
-      )
+      <form className={contactForm}>
+        <input type='text' placeholder='Name' value={this.props.contact.firstName}/>
+        <input type='text' placeholder='Last name' value={this.props.contact.lastName}/>
+        <input type='text' placeholder='email' value={this.props.contact.email}/>
+        <button type='submit'>Add contact</button>
+      </form>
+      // React.createElement('form', {className: 'contactForm'},
+      //   React.createElement('input', {
+      //     type: 'text',
+      //     placeholder: 'Name',
+      //     value: this.props.contact.firstName
+      //   }),
+      //   React.createElement('input', {
+      //     type: 'text',
+      //     placeholder: 'Last name',
+      //     value: this.props.contact.lastName
+      //   }),
+      //   React.createElement('input', {
+      //     type: 'text',
+      //     placeholder: 'email',
+      //     value: this.props.contact.email
+      //   }),
+      //   React.createElement('button', {type: 'submit'}, 'Add contact')
     )
   }
 })
