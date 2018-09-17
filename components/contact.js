c6e626d792de41e521272572950152b4443be464
Adam Pawliński <adam.pawlinski@gmail.com>
2018-09-17 05:39:52 +0200

    old scripts removed, only JSX stays

--- components/contact.js
+++ components/contact.js
@@ -10,14 +10,6 @@ var Contact = React.createClass({
         <p className="contactLabel">Last Name: {this.props.item.lastName}</p>
         <a className="contactEmail" href={this.props.item.email}>{this.props.item.email}</a>
       </div>
-      // React.createElement('div', {className: 'contactItem'},
-      //   React.createElement('img', {
-      //     className: 'contactImage',
-      //     src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico',
-      //   }),
-      //   React.createElement('p', {className: 'contactLabel'}, 'Name: ' + this.props.item.firstName),
-      //   React.createElement('p', {className: 'contactLabel'}, 'Last Name: ' + this.props.item.lastName),
-      //   React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email}, this.props.item.email),
     )
   }
 })
--- components/contactForm.js
+++ components/contactForm.js
@@ -11,23 +11,6 @@ var ContactForm = React.createClass({
         <input type='text' placeholder='email' value={this.props.contact.email}/>
         <button type='submit'>Add contact</button>
       </form>
-      // React.createElement('form', {className: 'contactForm'},
-      //   React.createElement('input', {
-      //     type: 'text',
-      //     placeholder: 'Name',
-      //     value: this.props.contact.firstName
-      //   }),
-      //   React.createElement('input', {
-      //     type: 'text',
-      //     placeholder: 'Last name',
-      //     value: this.props.contact.lastName
-      //   }),
-      //   React.createElement('input', {
-      //     type: 'text',
-      //     placeholder: 'email',
-      //     value: this.props.contact.email
-      //   }),
-      //   React.createElement('button', {type: 'submit'}, 'Add contact')
     )
   }
 })
